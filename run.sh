d9c73fd6e82b037e433cdb0cf53e5effc2ecdce4
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-14 18:30:08 +0900

    hoge

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 # input : input.gam, input.vg -> ouput.gam
 c++ -std=c++11 gamgenerator.cpp -o gamgenerator -O2
-python3 gam2csv.py $1 > $1.txt
-./gamgenerator $1
-python3 csv2json.py $1
-vg view -a -J $1.json -G > $1_testhaplo.gam
+time python3 gam2csv.py $1 > $1.txt
+echo "Finish GAM2CSV"
+time ./gamgenerator $1
+echo "Finish Generating"
+time python3 csv2json.py $1
+echo "Finish CSV2JSON"
+time vg view -a -J $1.json -G > $1_testhaplo.gam
+echo "Finish JSON2GAM"
